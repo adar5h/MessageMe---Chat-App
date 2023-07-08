65846379ed370df7b25e7254418bddbbae3fdb74
Adarsh <iadarshh@gmail.com>
2023-07-08 18:30:43 +0530

    flash messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,7 @@
 
 $(document).on('turbolinks:load', function(){ // letting the DOM load
     $('.ui.dropdown').dropdown();
+    $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+    });
 })
