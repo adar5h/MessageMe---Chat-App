17e1bfa360f4062f5951a3bb7d2676f98c440b3b
Adarsh <iadarshh@gmail.com>
2023-07-09 02:08:36 +0530

    implemented WebSocket protocol

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,19 @@
 class MessagesController < ApplicationController
-    
+
     def create
          message = current_user.messages.build(message_params)
          if message.save
-            redirect_to root_path
+            ActionCable.server.broadcast "chatroom_channel", 
+                                          mod_message: render_message(message)
          end
     end
 
     private
         def message_params
             params.require(:message).permit(:body)
+        end
+
+        def render_message(msg)
+            render(partial: 'message', locals: {message: msg})
         end
 end
